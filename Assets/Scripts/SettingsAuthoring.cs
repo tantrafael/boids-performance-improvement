cf25ba8c74a3274777ed14e10053f6f8821555d0
Rafael Hedman <rafael@rafael.se>
2024-06-02 06:52:22 +0200

    Refactor settings

--- Assets/Scripts/SettingsAuthoring.cs
+++ Assets/Scripts/SettingsAuthoring.cs
@@ -6,35 +6,16 @@ namespace Boids
 {
 	public class SettingsAuthoring : MonoBehaviour
 	{
-		/*
-		public int roundWorldSizeToMultiplesOf;
-		public float boidDensity;
 		public int boidCount;
+		public float boidDensity;
+		public int worldSizeRoundingIncrement;
 		public float initialSpeed;
 		public float viewRange;
 		public float matchRate;
 		public float coherenceRate;
 		public float avoidanceRange;
 		public float avoidanceRate;
-		// public float[] thrustTable;
-		public float thrustTeamRed;
-		public float thrustTeamGreen;
-		public float thrustTeamBlue;
-		public float dragTeamRed;
-		public float dragTeamGreen;
-		public float dragTeamBlue;
-		public GameObject boidPrefab;
-		*/
-		public int boidCount;
-		public float boidDensity;
-		public int roundWorldSizeToMultiplesOf;
-		public float initialSpeed;
-		public float viewRange;
-		public float matchRate;
-		public float coherenceRate;
-		public float avoidanceRange;
-		public float avoidanceRate;
-		// public float[] thrustTable;
+		// TODO: Organize team settings in lists.
 		public float thrustTeamRed;
 		public float thrustTeamGreen;
 		public float thrustTeamBlue;
@@ -49,46 +30,17 @@ namespace Boids
 			{
 				var entity = GetEntity(TransformUsageFlags.None);
 
-				const float exponent = 1.0f / 3.0f;
-				var a = math.pow(authoring.boidCount, exponent);
-				var b = authoring.boidDensity / authoring.roundWorldSizeToMultiplesOf;
-				var c = math.ceil(a * b);
-				var worldSize = c * authoring.roundWorldSizeToMultiplesOf;
-
-				/*
 				var settings = new Settings
 				{
-					// RoundWorldSizeToMultiplesOf = authoring.roundWorldSizeToMultiplesOf,
-					// BoidDensity = authoring.boidDensity,
 					BoidCount = authoring.boidCount,
+					WorldSize = GetWorldSize(authoring.boidCount, authoring.boidDensity, authoring.worldSizeRoundingIncrement),
 					InitialSpeed = authoring.initialSpeed,
 					ViewRange = authoring.viewRange,
 					MatchRate = authoring.matchRate,
 					CoherenceRate = authoring.coherenceRate,
 					AvoidanceRange = authoring.avoidanceRange,
 					AvoidanceRate = authoring.avoidanceRate,
-					// ThrustTable = authoring.thrustTable,
-					ThrustTeamRed = authoring.thrustTeamRed,
-					ThrustTeamGreen = authoring.thrustTeamGreen,
-					ThrustTeamBlue = authoring.thrustTeamBlue,
-					DragTeamRed = authoring.dragTeamRed,
-					DragTeamGreen = authoring.dragTeamGreen,
-					DragTeamBlue = authoring.dragTeamBlue,
-					BoidPrefab = GetEntity(authoring.boidPrefab, TransformUsageFlags.Dynamic),
-					// WorldSize = 40.0f
-					WorldSize = worldSize
-				};
-				*/
-				var settings = new Settings
-				{
-					BoidCount = authoring.boidCount,
-					WorldSize = worldSize,
-					InitialSpeed = authoring.initialSpeed,
-					ViewRange = authoring.viewRange,
-					MatchRate = authoring.matchRate,
-					CoherenceRate = authoring.coherenceRate,
-					AvoidanceRange = authoring.avoidanceRange,
-					AvoidanceRate = authoring.avoidanceRate,
+					// TODO: Organize team settings in lists.
 					ThrustTeamRed = authoring.thrustTeamRed,
 					ThrustTeamGreen = authoring.thrustTeamGreen,
 					ThrustTeamBlue = authoring.thrustTeamBlue,
@@ -100,31 +52,20 @@ namespace Boids
 
 				AddComponent(entity, settings);
 			}
+
+			private float GetWorldSize(int boidCount, float boidDensity, int worldSizeRoundingIncrement)
+			{
+				const float exponent = 1.0f / 3.0f;
+				var power = math.pow(boidCount, exponent);
+				var quotient = boidDensity / worldSizeRoundingIncrement;
+				var ceiling = math.ceil(power * quotient);
+				var worldSize = ceiling * worldSizeRoundingIncrement;
+
+				return worldSize;
+			}
 		}
 	}
 
-	/*
-	public struct Settings : IComponentData
-	{
-		// public int RoundWorldSizeToMultiplesOf;
-		// public float BoidDensity;
-		public int BoidCount;
-		public float InitialSpeed;
-		public float ViewRange;
-		public float MatchRate;
-		public float CoherenceRate;
-		public float AvoidanceRange;
-		public float AvoidanceRate;
-		public float ThrustTeamRed;
-		public float ThrustTeamGreen;
-		public float ThrustTeamBlue;
-		public float DragTeamRed;
-		public float DragTeamGreen;
-		public float DragTeamBlue;
-		public Entity BoidPrefab;
-		public float WorldSize;
-	}
-	*/
 	public struct Settings : IComponentData
 	{
 		public int BoidCount;
